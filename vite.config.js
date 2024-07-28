bc0a31d0f2acd9b930e9ef59972a2efb626ddc91
aenoriss <aenoriss@gmail.com>
2024-07-28 14:10:41 -0300

    FIX: Mediapipe workaround import

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { mediapipe_workaround } from './src/utils/mediapipe-workaround';
+import { mediapipe_workaround } from './src/Utils/mediapipe-workaround';
 
 export default defineConfig({
   plugins: [react()],
